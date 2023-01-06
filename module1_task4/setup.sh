15b15aaed98096c6ecd0c1aca03b45772a44d3f4
wael <waelbessaies2@gmail.com>
2023-01-06 21:05:48 +0100

    task4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+## Install Hugo && make
+
 apt-get update && apt-get install -y hugo make
 # getting latest release of HUGO
 wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb	
