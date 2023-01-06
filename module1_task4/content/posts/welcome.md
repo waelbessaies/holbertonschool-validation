ad815ffb9f96a810a07294ade769c12323d96553
wael <waelbessaies2@gmail.com>
2023-01-06 21:02:13 +0100

    task4

---
+++ module1_task4/content/posts/welcome.md
@@ -0,0 +1,6 @@
+---
+title: "Welcome to Awesome Inc."
+date: 2023-01-06T20:31:17+01:00
+draft: true
+---
+
---
+++ module1_task4/setup.sh
@@ -0,0 +1,7 @@
+apt-get update && apt-get install -y hugo make
+# getting latest release of HUGO
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb	
+# install it
+apt-get install ./hugo.deb
+# build the website
+make build
